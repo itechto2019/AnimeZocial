459695b50319fed9246119da0a3e6839b67f6717
Keuriseutian15 <keuriseutian15@gmail.com>
2022-07-28 05:38:53 +0800

    Update settings.py

--- AnimeZocial/settings.py
+++ AnimeZocial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4%&55y3chrft80*4*$s6865$z2&(a3ecnta2oifi7#kjmedbzy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
